5e5363bbccbd2136c16f50cdd24c6dd0689e3204
ro1611 <77914120+ro1611@users.noreply.github.com>
2021-07-13 23:29:10 +0530

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -1,34 +1,3 @@
-# resource "aws_security_group" "demo-sg" {
-#   name = "sg_22"
-#   vpc_id = "${aws_vpc.demo-vpc.id}"
-
-#   ingress {
-#       from_port   = 22
-#       to_port     = 22
-#       protocol    = "tcp"
-#       cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#       from_port   = 80
-#       to_port     = 80
-#       protocol    = "tcp"
-#       cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 65535
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Name = "Demo SG"
-#     Environment = terraform.workspace
-#   }
-# }
-
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "Ingress and Egress Rules"
